bd2135d1205690d93f7136a6a4ba1af9c58f5d77
gnb7456 <gnb7456@naver.com>
2019-07-15 17:45:58 +0900

    20190715 수업최종본

--- basic/test.py
+++ basic/test.py
@@ -1,2 +1,8 @@
 print('hi')
 print("hi")
+food="Python's favorite\n \"food\" is perl"
+print(food[5:])
+a=6.6666666
+print('i eat %8f apples.' %a)
+
+print("Python is {1} favor{0}ite".format(12,20))
