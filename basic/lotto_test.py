eb98a44fbcf65984253a4cde288dca76e99d55e6
gnb7456 <gnb7456@naver.com>
2019-07-17 14:33:06 +0900

    20190717 수업

--- basic/lotto_test.py
+++ basic/lotto_test.py
@@ -1,5 +1,15 @@
 import random
-rn=''
-while = True:
-    rn=random.randint(1,45)
-    for(rn)
+
+# 예시1
+#for i in range(5):
+#    lotto = [0,0,0,0,0,0]
+#   for x in range(6):
+#        num=0
+#        while(num in lotto):
+#            num=random.randint(1,45)
+#        lotto[x]=num
+#    print("로또 : ", sorted(lotto))
+
+# 예시2
+for i in range(1,6):
+    print("로또: ", sorted(random.sample(range(1,46), 6)))
